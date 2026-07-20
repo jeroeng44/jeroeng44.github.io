a71feb83e536c9cffd0dfd8a50fca7f95a9aa05e
jeroeng44 <126883680+jeroeng44@users.noreply.github.com>
2026-07-20 14:12:46 +0200

    Configure personal site for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://jeroeng44.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
